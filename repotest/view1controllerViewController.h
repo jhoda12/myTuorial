dd58a830a39dae4055d0dae4218737c1a7cb0da2
jhoda12 <jhoda12@hotmail.com>
2013-04-14 16:01:49 -0700

    generate view controller class for view 1

---
+++ repotest/view1controllerViewController.h
@@ -0,0 +1,16 @@
+//
+//  view1controllerViewController.h
+//  repotest
+//
+//  Created by Jhonathan on 4/14/13.
+//  Copyright (c) 2013 Jhonathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface view1controllerViewController : UIViewController
+
+- (IBAction)button1:(id)sender;
+
+
+@end
---
+++ repotest/view1controllerViewController.m
@@ -0,0 +1,40 @@
+//
+//  view1controllerViewController.m
+//  repotest
+//
+//  Created by Jhonathan on 4/14/13.
+//  Copyright (c) 2013 Jhonathan. All rights reserved.
+//
+
+#import "view1controllerViewController.h"
+
+@interface view1controllerViewController ()
+
+@end
+
+@implementation view1controllerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)button1:(id)sender {
+}
+@end
